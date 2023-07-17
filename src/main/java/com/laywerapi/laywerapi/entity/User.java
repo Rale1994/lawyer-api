4b344bc5fdb2f084460aa8388cc2cd8fe88e6051
unknown <golrale3@gmail.com>
2023-07-17 13:35:35 +0200

    modicitaded User

--- src/main/java/com/laywerapi/laywerapi/entity/User.java
+++ src/main/java/com/laywerapi/laywerapi/entity/User.java
@@ -24,6 +24,7 @@ public class User {
     private String username;
     private String password;
     private String role;
+    private boolean isEnable = false;
     @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL)
     private List<Client> clients;
 
