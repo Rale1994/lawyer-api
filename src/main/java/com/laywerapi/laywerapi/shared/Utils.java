9c14e32d77eb21677e2fe3ddc9cec2601d5aae2f
unknown <golrale3@gmail.com>
2023-08-03 08:36:59 +0200

    modification utils class

--- src/main/java/com/laywerapi/laywerapi/shared/Utils.java
+++ src/main/java/com/laywerapi/laywerapi/shared/Utils.java
@@ -9,37 +9,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Utils {
-    public User checkingForUpdatesUser(User user, UserUpdateRequestDTO userUpdateRequestDTO) {
-        if (StringUtils.isNoneBlank(userUpdateRequestDTO.getFirstName())) {
-            user.setFirstName(userUpdateRequestDTO.getFirstName());
-        }
-        if (StringUtils.isNoneBlank(userUpdateRequestDTO.getLastName())) {
-            user.setLastName(userUpdateRequestDTO.getLastName());
-        }
-        if (StringUtils.isNoneBlank(userUpdateRequestDTO.getPhone())) {
-            user.setPhone(userUpdateRequestDTO.getPhone());
-        }
-        if (StringUtils.isNoneBlank(userUpdateRequestDTO.getUsername())) {
-            user.setUsername(userUpdateRequestDTO.getUsername());
-        }
-        return user;
-    }
-
-    public Client checkingForUpdatesClient(Client client, ClientRequestDTO clientRequestDTO) {
-        if (StringUtils.isNoneBlank(clientRequestDTO.getFirstName())) {
-            client.setFirstName(clientRequestDTO.getFirstName());
-        }
-        if (StringUtils.isNoneBlank(clientRequestDTO.getLastName())) {
-            client.setLastName(clientRequestDTO.getLastName());
-        }
-        if (StringUtils.isNoneBlank(clientRequestDTO.getEmail())) {
-            client.setEmail(clientRequestDTO.getEmail());
-        }
-        if (StringUtils.isNoneBlank(clientRequestDTO.getPhone())) {
-            client.setPhone(clientRequestDTO.getPhone());
-        }
-        return client;
-    }
     public <T, U> T checkingForUpdates(T entity, U requestDTO) {
         if (entity instanceof User && requestDTO instanceof UserUpdateRequestDTO) {
             User user = (User) entity;
@@ -81,4 +50,36 @@ public class Utils {
             throw new IllegalArgumentException("Invalid entity or requestDTO types.");
         }
     }
+//    public User checkingForUpdatesUser(User user, UserUpdateRequestDTO userUpdateRequestDTO) {
+//        if (StringUtils.isNoneBlank(userUpdateRequestDTO.getFirstName())) {
+//            user.setFirstName(userUpdateRequestDTO.getFirstName());
+//        }
+//        if (StringUtils.isNoneBlank(userUpdateRequestDTO.getLastName())) {
+//            user.setLastName(userUpdateRequestDTO.getLastName());
+//        }
+//        if (StringUtils.isNoneBlank(userUpdateRequestDTO.getPhone())) {
+//            user.setPhone(userUpdateRequestDTO.getPhone());
+//        }
+//        if (StringUtils.isNoneBlank(userUpdateRequestDTO.getUsername())) {
+//            user.setUsername(userUpdateRequestDTO.getUsername());
+//        }
+//        return user;
+//    }
+//
+//    public Client checkingForUpdatesClient(Client client, ClientRequestDTO clientRequestDTO) {
+//        if (StringUtils.isNoneBlank(clientRequestDTO.getFirstName())) {
+//            client.setFirstName(clientRequestDTO.getFirstName());
+//        }
+//        if (StringUtils.isNoneBlank(clientRequestDTO.getLastName())) {
+//            client.setLastName(clientRequestDTO.getLastName());
+//        }
+//        if (StringUtils.isNoneBlank(clientRequestDTO.getEmail())) {
+//            client.setEmail(clientRequestDTO.getEmail());
+//        }
+//        if (StringUtils.isNoneBlank(clientRequestDTO.getPhone())) {
+//            client.setPhone(clientRequestDTO.getPhone());
+//        }
+//        return client;
+//    }
+
 }
